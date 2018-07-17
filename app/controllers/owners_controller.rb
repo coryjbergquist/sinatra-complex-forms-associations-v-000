21f0d9b3ec09b4f94f0ce22953484894935f01ec
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-17 03:06:53 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -23,6 +23,11 @@ class OwnersController < ApplicationController
     erb :'/owners/edit'
   end
 
+  get "/owners/:id" do 
+    erb :show
+  end
+  
+
   post '/owners/:id' do
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
