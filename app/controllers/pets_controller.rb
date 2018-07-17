46058b1bc21bff063195de9708ba4b1c0136c294
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-07-17 03:13:27 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -33,7 +33,6 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(name: params[:pet_name])
-    binding.pry
      if !params[:owner][:id].empty?
        @pet.update(owner_id: params[:owner][:id])
      elsif !params[:owner][:name].empty?
